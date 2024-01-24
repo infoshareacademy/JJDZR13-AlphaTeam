c345d0d58a7356468a206cbe3bd1be19338f3964
Michał Skoczylas <m.skoczylas1996@gmail.com>
2024-01-24 19:42:06 +0100

    first commit

--- src/test/java/MainMichal.java
+++ src/test/java/MainMichal.java
@@ -1,2 +1,30 @@
+import pl.isa.alphateam.Address;
+import pl.isa.alphateam.Boat;
+import pl.isa.alphateam.Customer;
+import pl.isa.alphateam.Reservation;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class MainMichal {
+    public static void main(String[] args) {
+
+        Address basiaAddress = new Address("KochanyKraj", "City", "Ulica", 12);
+        Customer basia = new Customer("Basia", "LName", "2024/01/20", "1234", basiaAddress, "basia@gmail.com", "pass");
+        Boat boatForBasia = new Boat("BoatName", 5, 100.5, true, "b1");
+        Reservation reservation1 = new Reservation(LocalDate.of(2024,1,4),
+                LocalDate.of(2024,1,11),
+                basia, boatForBasia);
+
+
+        System.out.println(basia);
+        double cost =reservation1.getCostOfReservation();
+        System.out.println(cost);
+
+        String uuid = reservation1.getUUID();
+
+
+
+
+    }
 }
