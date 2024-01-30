27b028716628770c37cbd3893b570d775f17e677
Basia <b4605634@gmail.com>
2024-01-30 20:15:58 +0100

    JJDZR13AT-15 menu

--- src/test/java/MainBasia.java
+++ src/test/java/MainBasia.java
@@ -1,19 +1,16 @@
-import pl.isa.alphateam.Boat;
-
-import java.util.List;
-
-import static pl.isa.alphateam.JSONParserBoat.getListOfBoatsFromDatabase;
-import static pl.isa.alphateam.JSONParserBoat.saveBoatInDatabase;
+import pl.isa.alphateam.Menu;
 
 public class MainBasia {
     public static void main(String[] args) {
-        List<Boat> boats = getListOfBoatsFromDatabase();
+  /*      List<Boat> boats = getListOfBoatsFromDatabase();
         for (Boat boat : boats) {
             System.out.println(boat.getName());
         }
 
         boolean written = saveBoatInDatabase(boats);
-        System.out.println(written);
+        System.out.println(written);*/
+
+        Menu.printMainPanelMenu();
     }
 
 }
