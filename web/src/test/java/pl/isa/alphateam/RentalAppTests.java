293fa4672269abf08e45aa2ff8ac0e22b20e0309
Tomasz Odwald <tomasz.odwald@gmail.com>
2024-03-03 18:55:43 +0100

    Added web module

---
+++ web/src/test/java/pl/isa/alphateam/RentalAppTests.java
@@ -0,0 +1,13 @@
+package pl.isa.alphateam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
